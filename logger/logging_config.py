6c982f4c6d5b3c45158453fcaf5b878210b15e71
Fabio Calefato <fcalefato@gmail.com>
2018-04-22 11:23:02 +0200

    add logging config file

---
+++ logger/logging_config.py
@@ -0,0 +1,35 @@
+import logging
+
+
+def get_logger(name=__file__, console_level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to INFO
+    ch = logging.StreamHandler()
+    ch.setLevel(console_level)
+    # create formatter
+    formatter = logging.Formatter('[%(asctime)s - %(name)s - %(levelname)s]: %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+    # file version of console output
+    chf = logging.FileHandler('console.log')
+    chf.setLevel(console_level)
+    chf.setFormatter(formatter)
+    logger.addHandler(chf)
+
+    # create error file handler and set level to WARNING
+    eh = logging.FileHandler('error.log')
+    eh.setLevel(logging.WARNING)
+    eh.setFormatter(formatter)
+    logger.addHandler(eh)
+
+    # create debug file handler and set level to DEBUG
+    dh = logging.FileHandler('debug.log')
+    dh.setLevel(logging.DEBUG)
+    dh.setFormatter(formatter)
+    logger.addHandler(dh)
+
+    return logger
