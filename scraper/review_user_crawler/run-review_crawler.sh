aa5692bb4a55241d1ea144d0ddc66cb85909740c
Fabio Calefato <fcalefato@gmail.com>
2018-06-13 11:40:17 +0200

    Add shell launchers for running scrapers directly

---
+++ scraper/review_user_crawler/run-review_crawler.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PYTHONPATH=$PYTHONPATH:.:../:../../:../../ph_py/:../../scraper
+python run-review_crawler.py 
---
+++ scraper/review_user_crawler/run-user_crawler.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PYTHONPATH=$PYTHONPATH:.:../:../../:../../ph_py/:../../scraper
+python run-user_crawler.py 
