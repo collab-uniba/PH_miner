9db8329da6630bcee47281f524493a9bca0c00b1
Fabio Calefato <fcalefato@gmail.com>
2018-06-13 09:40:43 +0200

    Fix issue in howto comment

--- cronjob.sh
+++ cronjob.sh
@@ -6,16 +6,16 @@
 #   SHELL=bash
 # New products are uploaded at 12.01 PST (just past midnight, 9.00am next morning in Italy):
 # minute hour day-of-month month day-of-week command
-#  45      8       *          *       *       /path/.../to/PH_miner/run.sh >> /var/log/ph_miner.log 2>&1
+#  45      8       *          *       *       /path/.../to/PH_miner/cronjob.sh >> /var/log/ph_miner.log 2>&1
 #
 # To scan also newest product that do NOT get featured, run every 15 min:
-#  */5     *       *          *       *       /path/.../to/PH_miner/run.sh --newest >> /var/log/ph_miner.log 2>&1
+#  */15     *       *          *       *       /path/.../to/PH_miner/cronjob.sh --newest >> /var/log/ph_miner.log 2>&1
 #
 # To enable the rotation of logs, run:
 #   $ sudo ln -s ph_miner.logrotate /etc/logrotate.d/ph_miner
 
 cd "$(dirname "$0")";
-# modify the path to the virtualenv folder
+# change the path to the virtualenv folder
 # instead, for conda, replace with:
 # source activate <environment name>
 source .env/bin/activate
