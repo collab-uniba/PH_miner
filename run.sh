e3758cf61554f9a3e1e0693d23e5bdbeeb524c71
Fabio Calefato <fcalefato@gmail.com>
2018-06-22 10:33:59 +0200

    Modify run.sh to check for and log exit code at each execution

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 export PYTHONPATH=$PYTHONPATH:.:./ph_py/:./scraper
 python ph_miner.py $1
+# check the exit code of the mining process
+# anything != 0 is error
+exit_code=$?
+DONE_FILE="./done.txt"
+ERRORED_FILE="./errored.txt"
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+if [ "$exit_code" -ne 0 ]; then
+    cat "$DATE" >> "$ERRORED_FILE"
+else
+    cat "$DATE" >> "$DONE_FILE"
+fi
+# for safety, kill chromedriver and google-chrome dangling processes
+killall -9 chrome > /dev/null
+killall -9 chromdriver > /dev/null
