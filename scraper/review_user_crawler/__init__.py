b1810600deb1af9dfe52c7e62167fda334183420
Fabio Calefato <fcalefato@gmail.com>
2018-07-24 09:50:57 +0200

    Improve comment

--- scraper/review_user_crawler/__init__.py
+++ scraper/review_user_crawler/__init__.py
@@ -1,10 +1,10 @@
 import os
 
+""" if vars are not set from the main app entry point, switch to default values """
 try:
     _ = os.environ['DB_CONFIG']
 except KeyError:
     os.environ['DB_CONFIG'] = '../../db/cfg/dbsetup.yml'
-
 try:
     _ = os.environ['PH_CREDENTIALS']
     pass
